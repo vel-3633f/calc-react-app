1530e7b99840976d5740e3ab3079b4cda5b4b86f
vel-3633f <vel3633fu@gmail.com>
2023-11-28 22:41:34 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-start">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,43 @@
+const btnItem = [
+  [7, 8, 9, "*"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, "AC", ".", "÷"],
+];
+
+const Button = () => {
+  return (
+    <div>
+      {btnItem.map((ary, index1) => {
+        return (
+          <div key={index1}>
+            {ary.map((item,index2) => {
+              return (
+                <button
+                  key={index2}
+                  className="px-2 py-1 m-0.5 h-16 w-16 text-yellow-500 border border-yellow-500 font-semibold rounded-full hover:bg-yellow-100"
+                >
+                  {item}
+                </button>
+              );
+            })}
+          </div>
+        );
+      })}
+      <button className="px-2 py-1 m-0.5 h-16 w-16 text-yellow-500 border border-yellow-500 font-semibold rounded-full hover:bg-yellow-100">=</button>
+    </div>
+  );
+};
+
+export default Button;
+
+// {
+//   ary.map((item, index) => {
+//     return (
+//       <div key={index}>
+//         <button>{item}</button>
+//       </div>
+//     );
+//   })
+// };
+// })}
