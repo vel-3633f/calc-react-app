a663d34e1e47457e8341097ef208193a01099476
vel-3633f <vel3633fu@gmail.com>
2023-11-29 21:27:22 +0900

    fix:shortened the code

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Button from "./components/Button";
 function App() {
   const [ans, setAns] = useState("");
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <p className="h-10">{ans}</p>
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-zinc-950">
+      <p className="h-10 text-white text-2xl">{ans}</p>
       <Button ans={ans} setAns={setAns} />
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,42 +5,27 @@ const btnItem = [
   [7, 8, 9, "÷"],
   [4, 5, 6, "*"],
   [1, 2, 3, "-"],
-  [0, "AC", ".", "+"],
+  [0, ".", "AC", "+"],
 ];
 const buttonStyle =
-  "px-2 py-1 m-0.5 h-16 w-16 text-white bg-gray-600 font-semibold rounded-full hover:bg-gray-400 disabled:bg-gray-300";
+  "px-2 py-1 m-0.5 h-16 w-16 text-white font-semibold rounded-full disabled:bg-yellow-200 text-2xl";
 
 const Button = ({ ans, setAns }) => {
   const [isCheck, setIsCheck] = useState(false);
 
-  const addAns = (e) => {
-    const newValue = e.target.textContent;
-    switch (newValue) {
+  const handleButtonClick = (value) => {
+    switch (value) {
       case "AC":
         setAns("");
-        break;
-      case "+":
-        setAns(ans + newValue);
-        setIsCheck(false);
-        break;
-      case "-":
-        setAns(ans + newValue);
-        setIsCheck(false);
-        break;
-      case "*":
-        setAns(ans + newValue);
-        setIsCheck(false);
-        break;
-      case "÷":
-        setAns(ans + newValue);
-        setIsCheck(false);
         break;
       case "=":
         setAns(calcFunc(ans));
         break;
       default:
-        setAns(ans + newValue);
-        setIsCheck(true);
+        setAns(ans + value);
+        setIsCheck(
+          value !== "+" && value !== "-" && value !== "*" && value !== "÷"
+        );
     }
   };
   return (
@@ -52,8 +37,12 @@ const Button = ({ ans, setAns }) => {
               return (
                 <button
                   key={index2}
-                  className={buttonStyle}
-                  onClick={addAns}
+                  className={`${buttonStyle} ${
+                    index2 === 3
+                      ? "bg-yellow-500 hover:bg-yellow-400"
+                      : "bg-gray-600 hover:bg-gray-400"
+                  }`}
+                  onClick={() => handleButtonClick(item)}
                   disabled={index2 === 3 ? !isCheck : false}
                 >
                   {item}
@@ -63,7 +52,10 @@ const Button = ({ ans, setAns }) => {
           </div>
         );
       })}
-      <button className={buttonStyle} onClick={addAns}>
+      <button
+        className={`${buttonStyle} bg-gray-600 hover:bg-gray-400"`}
+        onClick={() => handleButtonClick("=")}
+      >
         =
       </button>
     </div>
